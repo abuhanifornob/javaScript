ff7e3b3e3af75104b9a18a1a06eea874a7dda345
Abu Hanif Arnob <37977247+abuhanifornob@users.noreply.github.com>
2022-05-18 19:28:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    }
+
+function myTimeShow(){
+        document.getElementById("dateTime").innerHTML=Date();
+    }
+function onLighr(){
+    document.getElementById('myImage').src='images/pic_bulbon.gif';
+}
+function offLight(){
+    document.getElementById('myImage').src='images/pic_bulboff.gif';
+}
+
+ function styleChange(){
+     document.getElementById('cssPara').style.fontSize='50px';
+ }
+
+ function hideParagraph(){
+     document.getElementById('hidePara').style.display='none';
+ }
